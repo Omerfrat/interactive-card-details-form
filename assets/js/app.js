068b5b8561db318a540880664aa8091a7fd13599
Ömer Fırat <omrfrt16@gmail.com>
2024-06-06 16:05:09 +0300

    asdasd

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,4 +3,33 @@ let cardNumberInput = document.querySelector('.card-number1');
 let cardAaInput = document.querySelector('#aa');
 let cardYyInput = document.querySelector('#yy');
 let cardCvcInput = document.querySelector('.card-cvc');
+let gonder = document.querySelector('#submit');
 
+let ccName = document.querySelector('#card-name');
+let ccNumber = document.querySelector('#cc-number');
+let ccAa = document.querySelector('#card-aa');
+let ccYy = document.querySelector('#card-yy');
+let cvc = document.querySelector('#cvc');
+
+
+
+// gonder.addEventListener("click", (e)=>  {
+//   my
+//   e.preventDefault();
+//   ccName.innerText = cardNameInput.value;
+//   ccNumber.innerText = cardNumberInput.value;
+//   ccAa.innerText = cardAaInput.value;
+//   ccYy.innerText = cardYyInput.value;
+//   cvc.innerText = cardCvcInput.value;
+// })
+
+
+function myFunction() {
+  ccName.innerText = cardNameInput.value;
+  ccNumber.innerText = cardNumberInput.value;
+  ccAa.innerText = cardAaInput.value;
+  ccYy.innerText = cardYyInput.value;
+  cvc.innerText = cardCvcInput.value;
+}
+
+gonder.addEventListener("click", myFunction)
